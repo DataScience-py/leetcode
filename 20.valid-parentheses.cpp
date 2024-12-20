a4d150047256208a978797e41e985abb92499648
Anton Golubchikov <anton1programmist@gmail.com>
2024-12-20 18:41:34 +0700

    20, 21, 26 in cpp

---
+++ 20.valid-parentheses.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.empty())
+                    return false;
+                if (s[i] == ')' && st.top() == '(')
+                    st.pop();
+                else if (s[i] == '}' && st.top() == '{')
+                    st.pop();
+                else if (s[i] == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty(); // Return true if all parentheses are matched
+    }
+};
+// @lc code=end
