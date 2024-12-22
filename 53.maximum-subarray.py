6f25c0bd5bcb129839fec0584352cb70597f353b
Anton Golubchikov <anton1programmist@gmail.com>
2024-12-22 19:04:55 +0700

    27, 28, 53 add in python3

---
+++ 53.maximum-subarray.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=53 lang=python3
+#
+# [53] Maximum Subarray
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        max_sum = nums[0]
+        cur_sum = nums[0]
+        for i in range(1, len(nums)):
+            cur_sum = max(nums[i], cur_sum + nums[i])
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
+
+# @lc code=end
+
