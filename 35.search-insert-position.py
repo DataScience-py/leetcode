ddfd6c2c014c74df8d8eaad019bd529184c1da7a
Anton Golubchikov <anton1programmist@gmail.com>
2024-12-26 21:46:15 +0700

    35, 66, 88 in python3

---
+++ 35.search-insert-position.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=35 lang=python3
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        right = len(nums)
+        left = 0
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+        
+# @lc code=end
+
